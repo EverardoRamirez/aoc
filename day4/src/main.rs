81279bd33c3b0f7543e7294cef9cdf50da4a57dd
Everardo Ramirez Rosales <everardo.ramirez1@ibm.com>
2024-08-05 17:35:45 -0600

    Day4 finish

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
+    let input: &str = "bgvyzdsv";
+    let mut count: i128 = 0;
+    loop {
+        let test = format!("{input}{count}");
+        let digest = format!("{:x}", md5::compute(test.as_bytes()));
+        if digest.starts_with("000000") {
+            println!("{count}");
+            break;
+        }
+        count += 1;
+    }
+
     println!("Hello, world!");
 }
