ad8e38e9d0aa22bd020abcb7877fb2f33d22d3e1
Everardo Ramirez Rosales <everardo.ramirez1@ibm.com>
2024-08-06 14:31:36 -0600

    day5 part1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,39 @@
+use fancy_regex::Regex;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 fn main() {
-    println!("Hello, world!");
+    let path = "C:\\Users\\080576781\\Desktop\\rust\\aoc\\day5\\input.txt";
+    // let content =
+    //     read_to_string("C:\\Users\\080576781\\Desktop\\rust\\aoc\\day2\\input.txt").unwrap();
+
+    let file = File::open(path).unwrap();
+    let content = BufReader::new(file).lines();
+    let mut counter = 0;
+    for line in content {
+        let line = line.unwrap();
+        if has_3_vowels_or_more(&line) && has_twice_in_arow(&line) && !contains_prohibited(&line) {
+            println!("{line} is nice");
+            counter += 1;
+        } else {
+            println!("{line} is nasty");
+        }
+    }
+    println!("{counter}");
+}
+
+fn has_3_vowels_or_more(line: &str) -> bool {
+    let re = Regex::new(r"(.*(a|e|i|o|u).*){3,}").unwrap();
+    re.is_match(line).unwrap()
+}
+
+fn has_twice_in_arow(line: &str) -> bool {
+    let re = Regex::new(r".*(.)\1{1,}.*").unwrap();
+    re.is_match(line).unwrap()
+}
+
+fn contains_prohibited(line: &str) -> bool {
+    let re = Regex::new(r".*(ab)|(cd)|(pq)|(xy).*").unwrap();
+    re.is_match(line).unwrap()
 }
