b40749968d5b7bc149548d2c16fb545970fc88e7
Everardo Ramirez Rosales <everardo.ramirez1@ibm.com>
2024-08-09 15:59:54 -0600

    Day 6 part2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -11,26 +11,33 @@ fn main() {
     let file = File::open(path).unwrap();
     let content = BufReader::new(file).lines();
 
-    let mut matrix = [[false; SIDE]; SIDE];
+    let mut matrix = vec![[0_u32; SIDE]; SIDE];
     for line in content {
         let line = line.unwrap();
 
         if line.starts_with("turn on") {
             let points = Point::from_str(&line["turn on ".len()..]);
-            update_light(&mut matrix, &points.0, &points.1, |x| *x = true);
+            update_light(&mut matrix, &points.0, &points.1, |x| *x += 1);
         } else if line.starts_with("turn off") {
             let points = Point::from_str(&line["turn off ".len()..]);
-            update_light(&mut matrix, &points.0, &points.1, |x| *x = false);
+            update_light(&mut matrix, &points.0, &points.1, |x| {
+                if *x != 0 {
+                    *x -= 1
+                }
+            });
         } else if line.starts_with("toggle") {
             let points = Point::from_str(&line["toggle ".len()..]);
-            update_light(&mut matrix, &points.0, &points.1, |x| *x = !*x);
+            update_light(&mut matrix, &points.0, &points.1, |x| *x += 2);
         }
     }
 
-    let count = matrix.iter().flatten().filter(|x| **x).count();
-    matrix.iter().for_each(|x| println!("{:?}", x));
+    let count = matrix
+        .into_iter()
+        .flatten()
+        .reduce(|prev, current| prev + current)
+        .unwrap();
 
-    println!("{count}");
+    println!("{}", count);
 }
 
 #[derive(Debug)]
@@ -55,12 +62,7 @@ impl Point {
     }
 }
 
-fn update_light(
-    array: &mut [[bool; SIDE]; SIDE],
-    from: &Point,
-    to: &Point,
-    command: fn(&mut bool) -> (),
-) {
+fn update_light(array: &mut [[u32; SIDE]], from: &Point, to: &Point, command: fn(&mut u32) -> ()) {
     let rows = &mut array[from.y..=to.y];
     for row in rows {
         let colums = &mut row[from.x..=to.x];
